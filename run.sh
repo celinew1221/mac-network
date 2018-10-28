74a7d7c6f2f14417108c97b753b150a674e36186
Celine Wei <c.wei@surgicalsafety.com>
2018-10-27 21:57:04 -0400

    new

--- run.sh
+++ run.sh
@@ -4,25 +4,27 @@
 git checkout baseline
 # 1 - load weights
 echo "Fined-tune and Trained only on Action Data"
-python3 main.py --expName "action_only_finetuned" --train --epochs 200 --netLength 6 --gpus 0 --workers 1 --taskSize
-8 --batchSize 64 --weightsToKeep 20 --earlyStopping 20 --restoreEpoch 11 --incluAction --actionOnlyTrain @configs/args1.txt
+python3 main.py --expName "action_only_finetuned" --train --epochs 5 --netLength 6 --gpus 0 --workers 1 --taskSize
+8 --batchSize 64 --weightsToKeep 20 --earlyStopping 2 --restoreEpoch 11 --incluAction --actionOnlyTrain
+@configs/args1.txt --trainedNum 1000 --testedNum 1000
 python main.py --expName "action_only_finetuned" --finalTest --netLength 6 -r --getPreds --getAtt @configs/args1.txt
+--testedNum 1000
 python visualization.py --expName "action_only_finetuned" --tier val
 
-# 2 - from scratch
-echo "Action from Scratch"
-python3 main.py --expName "action_only_scratch" --train --epochs 200 --netLength 6 --gpus 0 --workers 1 --taskSize 8
---batchSize 64 --weightsToKeep 20  --earlyStopping 20  --incluAction --actionOnlyTrain @configs/args1.txt
-python main.py --expName "action_only_scratch" --finalTest --netLength 6 -r --getPreds --getAtt @configs/args1.txt
-python visualization.py --expName "action_only_scratch" --tier val
-
-# intraAction Cell
-echo "intraActionCell"
-git checkout intraActionCell
-python3 main.py --expName "intraActionCell" --train --epochs 200 --netLength 6 --gpus 0 --workers 1 --taskSize 8
---batchSize 64 --weightsToKeep 20  --earlyStopping 20 @configs/args1.txt
-python main.py --expName "intraActionCell" --finalTest --netLength 6 -r --getPreds --getAtt @configs/args1.txt
-python visualization.py --expName "intraActionCell" --tier val
+## 2 - from scratch
+#echo "Action from Scratch"
+#python3 main.py --expName "action_only_scratch" --train --epochs 200 --netLength 6 --gpus 0 --workers 1 --taskSize 8
+#--batchSize 64 --weightsToKeep 20  --earlyStopping 20  --incluAction --actionOnlyTrain @configs/args1.txt
+#python main.py --expName "action_only_scratch" --finalTest --netLength 6 -r --getPreds --getAtt @configs/args1.txt
+#python visualization.py --expName "action_only_scratch" --tier val
+#
+## intraAction Cell
+#echo "intraActionCell"
+#git checkout intraActionCell
+#python3 main.py --expName "intraActionCell" --train --epochs 200 --netLength 6 --gpus 0 --workers 1 --taskSize 8
+#--batchSize 64 --weightsToKeep 20  --earlyStopping 20 @configs/args1.txt
+#python main.py --expName "intraActionCell" --finalTest --netLength 6 -r --getPreds --getAtt @configs/args1.txt
+#python visualization.py --expName "intraActionCell" --tier val
 
 
 
