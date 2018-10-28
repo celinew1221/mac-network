5542b33cfb1bcd9f8cb7b27afb9f3312826770c1
Celine Wei <c.wei@surgicalsafety.com>
2018-10-27 21:50:11 -0400

    list of model to train

--- run.sh
+++ run.sh
@@ -1,7 +1,28 @@
-python3 main.py --expName "baseline_pre_trained6" --train --epochs 200 --netLength 6 --gpus 0 --workers 1 --taskSize 8 --batchSize 64 --weightsToKeep 10  @configs/args1.txt --earlyStopping 20 --restoreEpoch 1
-python3 main.py --expName "baseline_pre_trained12" --train --epochs 200 --netLength 12 --gpus 0 --workers 1 --taskSize 8 --batchSize 64 --weightsToKeep 10  @configs/args1.txt --earlyStopping 20 --restoreEpoch 1
-python3 main.py --expName "baseline_pre_trained_scratch" --train --epochs 200 --netLength 6 --gpus 0 --workers 1 --taskSize 8 --batchSize 64 --weightsToKeep 10  @configs/args1.txt --earlyStopping 20
+#!/usr/bin/env bash
 
+# baseline
+git checkout baseline
+# 1 - load weights
+echo "Fined-tune and Trained only on Action Data"
+python3 main.py --expName "action_only_finetuned" --train --epochs 200 --netLength 6 --gpus 0 --workers 1 --taskSize
+8 --batchSize 64 --weightsToKeep 20 --earlyStopping 20 --restoreEpoch 11 --incluAction --actionOnlyTrain @configs/args1.txt
+python main.py --expName "action_only_finetuned" --finalTest --netLength 6 -r --getPreds --getAtt @configs/args1.txt
+python visualization.py --expName "action_only_finetuned" --tier val
+
+# 2 - from scratch
+echo "Action from Scratch"
+python3 main.py --expName "action_only_scratch" --train --epochs 200 --netLength 6 --gpus 0 --workers 1 --taskSize 8
+--batchSize 64 --weightsToKeep 20  --earlyStopping 20  --incluAction --actionOnlyTrain @configs/args1.txt
+python main.py --expName "action_only_scratch" --finalTest --netLength 6 -r --getPreds --getAtt @configs/args1.txt
+python visualization.py --expName "action_only_scratch" --tier val
+
+# intraAction Cell
+echo "intraActionCell"
+git checkout intraActionCell
+python3 main.py --expName "intraActionCell" --train --epochs 200 --netLength 6 --gpus 0 --workers 1 --taskSize 8
+--batchSize 64 --weightsToKeep 20  --earlyStopping 20 @configs/args1.txt
+python main.py --expName "intraActionCell" --finalTest --netLength 6 -r --getPreds --getAtt @configs/args1.txt
+python visualization.py --expName "intraActionCell" --tier val
 
 
 
