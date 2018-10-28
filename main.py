c66afb3cec40e31adad2e1314c07cc6fb2b0021c
Celine Wei <c.wei@surgicalsafety.com>
2018-10-28 14:16:52 -0400

    flip main and extra in evaluation for action enable training

--- main.py
+++ main.py
@@ -730,7 +730,7 @@ def main():
 
                 # evaluation
                 config.logger.debug("Evaluate on both main and extra val")
-                if config.incluAction:
+                if config.actionOnlyTrain or config.alterActionTrain:
                     evalRes = runEvaluation(sess, model, data["extra"], epoch)
                     extraEvalRes = runEvaluation(sess, model, data["main"], epoch,
                                                  evalTrain=not config.extraVal)
@@ -801,7 +801,7 @@ def main():
                     saver.restore(sess, config.weightsFile(epoch))
 
             config.logger.debug("Test on main and extra testing set")
-            if config.incluAction:
+            if config.actionOnlyTrain or config.alterActionTrain:
                 evalRes = runEvaluation(sess, model, data["extra"], epoch, evalTest=True)
                 extraEvalRes = runEvaluation(sess, model, data["main"], epoch,
                                              evalTrain=not config.extraVal, evalTest=True)
